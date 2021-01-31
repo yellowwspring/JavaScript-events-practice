585f70a8755dbc2f86019cd55110195b4298771e
yellowwspring <yellowwspring@users.noreply.github.com>
2021-01-31 15:04:03 +0300

    Added examples with .onmousedown and .onmouseup

--- module-js-events/events-mouse/code-mouse.js
+++ module-js-events/events-mouse/code-mouse.js
@@ -16,6 +16,7 @@ block.ondblclick = function () {
 
 // мышь на объекте
 block.onmouseenter = function () {
+    console.log("Мышь на квадрате!")
     this.style.background = "pink";
 }
 
@@ -26,3 +27,18 @@ block.onmousemove = function (){
     a++;
     this.style.width = 100 + a + 'px';
 }
+
+// нажатие и удержание кнопки мыши(любой)
+let c = 0;
+block.onmousedown = function (event) {
+    console.log("Удерживаю!");
+    this.style.cursor = "crosshair"
+    console.log(event.button); // выдаст инормацию какая кнопка была нажата 0 - левая. 1 - колёсико . 2 - правая.
+}
+
+
+// отпускаю нажатие внутри блока
+block.onmouseup = function (){
+    this.style.width = "80px";
+}
+
